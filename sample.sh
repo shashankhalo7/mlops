c83713dee07942569f4b913616f3df7350a099f9
Ubuntu <ubuntu@ip-172-31-27-71.us-west-2.compute.internal>
2020-05-04 11:20:08 +0000

    init

--- sample.sh
+++ sample.sh
@@ -2,12 +2,14 @@
 mkdir /opt/ml/
 mkdir /opt/ml/model
 mkdir /opt/program/
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 git clone $GIT_REPO /opt/program
 wget -O /opt/ml/model/model.pkl $MODEL_URL
 
 echo $VAR1
 ls /opt/program/
 ls /opt/ml/model/
-set FLASK_APP = /opt/program/serve.py
-flask run
+export FLASK_APP=/opt/program/serve.py
+flask run 
 
